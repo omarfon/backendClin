e5ff0dd492e1db4b95c52dda4cd3efef0cd3de00
omar <fonseca>
2019-10-27 16:14:31 -0500

    problemas con mongoose

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 // requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-//conexcion base de datos
+//Inicializar variables
+var app = express();
+
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+
+
+//conexion base de datos, despues de inyectar mongoose, con esta linea hacemos la conexion.
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
         if(err) throw err;
         console.log('Base de datos: \x1b[32m%s\x1b[0m' , 'online');
 })
 
 
-
-//Inicializar variables
-var app = express();
-
 //Rutas
-app.get('/', (req, res, next) =>{
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-})
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
 
 
 //Escuchar peticiones
